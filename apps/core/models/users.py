780ae960bd87257b182b0c328309672d16906c6d
Dominik Lysiak <mail@lysiak.dev>
2025-04-04 14:38:32 +0200

    refactor: update translation function to use gettext_lazy

--- apps/core/models/users.py
+++ apps/core/models/users.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
